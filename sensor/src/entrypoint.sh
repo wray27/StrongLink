b761737deefdddb6c3a8fde5932bebd89d59fce8
Ubuntu <azureuser@StrongLinkVM.zptbobfnctqerczlyhdvhtz1lb.zx.internal.cloudapp.net>
2021-10-26 18:44:53 +0000

    Implemented code required to create the warehouse container. Also removed the src folder and refactored the code into a truck and warehouse folder

--- sensor/src/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-az login --service-principal -u $AZ_APP_URL -p $AZ_APP_SECRET --tenant $AZ_TENANT_ID
-az iot hub device-identity connection-string show --device-id $DEVICE_ID --hub-name $AZ_IOT_HUB
-export IOTHUB_DEVICE_CONNECTION_STRING="$(az iot hub device-identity connection-string show --device-id $DEVICE_ID \
-    --hub-name StrongLink-IoT-Hub | python3 -c "import sys, json; print(json.load(sys.stdin)['connectionString'])")"
-python3 simulation.py
